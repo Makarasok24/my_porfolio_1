65635904ac424096ff9b34e024e2d7afbacbe8f0
Makarasok16 <makarasok.it@gmail.com>
2024-06-14 15:19:29 +0700

    init2
    init_02

---
+++ facebook.js
@@ -0,0 +1,9 @@
+let mail = document.getElementById('mail');
+let password = document.getElementById('password');
+
+function login(){
+    let value = mail.value;
+    let valuePass = password.value;
+    console.log(value);
+    console.log(valuePass);
+}
